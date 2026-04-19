c91a13f11267b49350267d1d064533c54e9137c7
Tyler Fibonacci <gepheum@gmail.com>
2026-04-19 14:53:15 -0400

    support lowercased constant names

--- src/index.ts
+++ src/index.ts
@@ -478,8 +478,9 @@ class RustSourceFileGenerator {
               `(*a).add_wrapper_variant("${variant.name.text}", ${variant.number}, ${kindOrdinal}, ${serializerExpr}, ${toRustStringLiteral(docToCommentText(variant.doc))}, ${wrapFn}, ${getValueFn});\n`,
             );
           } else {
+            const skirName = convertCase(variant.name.text, "UPPER_UNDERSCORE");
             this.push(
-              `(*a).add_constant_variant("${variant.name.text}", ${variant.number}, ${kindOrdinal}, ${toRustStringLiteral(docToCommentText(variant.doc))}, ${typeName}::${variantName});\n`,
+              `(*a).add_constant_variant("${skirName}", ${variant.number}, ${kindOrdinal}, ${toRustStringLiteral(docToCommentText(variant.doc))}, ${typeName}::${variantName});\n`,
             );
           }
           kindOrdinal++;
